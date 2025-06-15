43d681992c49914f14474b62786afecd7f0371fd
Andrii Sozonik <sozonik04.av@gmail.com>
2025-06-14 17:02:47 -0700

    backend: allow requests from wishy.aandrku.dev

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -4,10 +4,19 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"https://wishy.aandrku.dev"},
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	}))
+
 	e.GET("/", func(c echo.Context) error {
 		m := struct {
 			Message string `json:"message"`
