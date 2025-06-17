84adcaec0d1026f6f5d090e7da19d60f4b5269b8
Andrii Sozonik <sozonik04.av@gmail.com>
2025-06-16 18:04:13 -0700

    add favicon.ico and use gift from lucide icons as logo

--- frontend/src/components/pages/login-page.tsx
+++ frontend/src/components/pages/login-page.tsx
@@ -1,6 +1,5 @@
-import { GalleryVerticalEnd } from 'lucide-react';
-
 import { LoginForm } from '@/components/shared/login-form';
+import { Gift } from 'lucide-react';
 
 export default function LoginPage() {
   return (
@@ -8,7 +7,7 @@ export default function LoginPage() {
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+            <Gift></Gift>
           </div>
           Wishy
         </a>
