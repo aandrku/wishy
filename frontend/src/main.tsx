b5a0598908448a64e8313ffcdcf85db059e90c19
Andrii Sozonik <sozonik04.av@gmail.com>
2025-06-26 15:23:12 -0700

    frontend: start fresh

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import App from './App.tsx';
-import LoginPage from './components/pages/login-page.tsx';
-import SignupPage from './components/pages/signup-page.tsx';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/signup" element={<SignupPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <App></App>
   </StrictMode>
 );
